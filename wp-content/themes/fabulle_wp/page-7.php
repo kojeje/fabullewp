addf0ea93584422b399e961c700fdadc72d8d63f
kojeje <jerome.suhard@lapiscine.pro>
2019-04-22 19:12:50 +0200

    page cie et contact et responsive Boucles ok

--- wp-content/themes/fabulle_wp/page-7.php
+++ wp-content/themes/fabulle_wp/page-7.php
@@ -14,30 +14,30 @@
 // tableau d'arguments pour modifier la requête en base
 // de données, et venir récupérer uniquement les trois
 // derniers articles
-  $args_articles = [
-    'post_type' => 'post',
-    'category_name' => 'home'
-
-  ];
 
   $args_articles2 =[
-    'post-type' => 'post',
-    'category_name' => 'show',
+    'post-type' => 'shows',
+
   ];
 
   $args_articles3 = [
-    'post-type' => 'post',
-    'category_name' => 'event',
-    'post_per_page' => 5
+    'post-type' => 'events',
+    'post_per_page' => 10
   ];
 
 // récupère les articles en fonction du tableau d'argument $args_posts
 // en utilisant la méthode de Timber get_posts
 // puis on les enregistre dans l'array $context sous la clé "posts"
-  $context['homes'] = Timber::get_posts($args_articles);
+
   $context['shows'] = Timber::get_posts($args_articles2);
   $context['events'] = Timber::get_posts($args_articles3);
+
+//  var_dump($context['shows']);die;
+
 
 // appelle la vue twig "index.twig" située dans le dossier views
 // en lui passant la variable $context qui contient notamment ici les articles
   Timber::render('page-7.twig', $context);
+
+  $twig = new \Twig\Environment($loader);
+  $twig->getExtension('\Twig\Extension\CoreExtension')->setTimezone('Europe/Paris');
